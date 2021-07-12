2c54d44ca83f3facd7845585ece1765791a5242b
mic <mihailescucristina@outlook.com>
2021-07-12 16:16:30 +0300

    + global metrics menu link

--- src/components/nav/header/NavItem/index.js
+++ src/components/nav/header/NavItem/index.js
@@ -1,15 +1,53 @@
-import { Typography, makeStyles, useMediaQuery } from '@material-ui/core';
+import {
+    Typography,
+    makeStyles,
+    useMediaQuery,
+    useTheme,
+} from '@material-ui/core';
 import { Clear } from '@material-ui/icons';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import Button from '../../../buttons/Button';
 import SearchBar from '../SearchBar';
 import Logo from '../Logo';
 import Text from '../../../text/Text';
 import { useStyles } from './styles';
+import { useEffect } from 'react';
 
 const NavList = ({ toggleDrawer }) => {
     const styles = useStyles();
     const resolutionIsXS = useMediaQuery(theme => theme.breakpoints.only('xs'));
+
+    const location = useLocation();
+    const theme = useTheme();
+    useEffect(() => {
+        console.log(location.pathname);
+    }, [location]);
+
+    const NavItem = ({ text, path }) => {
+        console.log(
+            location.pathname,
+            path,
+            location.pathname === path,
+            'here'
+        );
+        return (
+            <li className={styles.navItem + ' ' + styles.navLinkWrapper}>
+                <Link to={path} className={styles.navLink}>
+                    <Text
+                        size="large"
+                        style={{
+                            color:
+                                path === location.pathname
+                                    ? theme.palette.text.accentPink
+                                    : theme.palette.text.primary,
+                        }}
+                    >
+                        {text}
+                    </Text>
+                </Link>
+            </li>
+        );
+    };
 
     return (
         <ul className={styles.navList}>
@@ -26,16 +64,9 @@ const NavList = ({ toggleDrawer }) => {
                     </div>
                 </>
             )}
-            <li className={styles.navItem + ' ' + styles.navLinkWrapper}>
-                <Link to="/home" className={styles.navLink}>
-                    <Text size="large">Home</Text>
-                </Link>
-            </li>
-            <li className={styles.navItem + ' ' + styles.navLinkWrapper}>
-                <Link to="/news" className={styles.navLink}>
-                    <Text size="large">News</Text>
-                </Link>
-            </li>
+            <NavItem text="Home" path="/home" />
+            <NavItem text="News" path="/news" />
+            <NavItem text="Global Metrics" path="/global-metrics" />
             {!resolutionIsXS && (
                 <div className={styles.navSearchBar}>
                     <SearchBar />
